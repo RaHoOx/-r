b7819dffdcde7edac3b368370c6b41668ee7500a
abohazhar2020 <76555352+abohazhar2020@users.noreply.github.com>
2021-01-22 23:26:49 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const db = require('quick.db');
 const { TOKEN, PREFIX, AVATARURL, BOTNAME, } = require(`./config.json`);
 const figlet = require("figlet");
 const client = new Client({ disableMentions: `` , partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });
-client.login('NzY5MzE2ODIwOTkyNjU1Mzkx.X5NQHw.zSGp-ftXb0uGMgH5umA5Gx4-fcU');
+client.login('NzY5MzE2ODIwOTkyNjU1Mzkx.X5NQHw.YjN2czxBR2hS8slibMTyA9iAsn4');
 client.commands = new Collection();
 client.setMaxListeners(0);
 client.prefix = PREFIX;
